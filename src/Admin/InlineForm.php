90c291b0782fed28a454233ca1243ae81b0b4568
Frescoref <frescoref@gmail.com>
2026-08-07 11:46:52 +0300

    Fix: more

--- src/Admin/InlineForm.php
+++ src/Admin/InlineForm.php
@@ -52,7 +52,7 @@ class InlineForm extends FormAbstract
 
         $data_process = true;
 
-        if(empty($post_data) || !isset($post_data[$data_key]) || !wp_verify_nonce(sanitize_text_field(wp_unslash($post_data[$data_key])), 'wc1c-admin-' . $this->getId() . '-save'))
+        if(empty($post_data) || !wp_verify_nonce(sanitize_key(wp_unslash($post_data[$data_key])), 'wc1c-admin-' . $this->getId() . '-save'))
         {
             $data_process = false;
         }
