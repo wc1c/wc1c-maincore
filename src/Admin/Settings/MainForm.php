86febf2ca270006ab7204356b30922cadfe18b29
Frescoref <frescoref@gmail.com>
2026-08-07 10:03:29 +0300

    Fix: more

--- src/Admin/Settings/MainForm.php
+++ src/Admin/Settings/MainForm.php
@@ -40,35 +40,35 @@ class MainForm extends Form
 	{
 		$fields['configurations_title'] =
 		[
-			'title' => __('Configurations', 'wc1c-maincore'),
+			'title' => esc_html__('Configurations', 'wc1c-maincore'),
 			'type' => 'title',
-			'description' => __('Some settings for the configurations.', 'wc1c-maincore'),
+			'description' => esc_html__('Some settings for the configurations.', 'wc1c-maincore'),
 		];
 
 		$fields['configurations_unique_name'] =
 		[
-			'title' => __('Unique names', 'wc1c-maincore'),
+			'title' => esc_html__('Unique names', 'wc1c-maincore'),
 			'type' => 'checkbox',
-			'label' => __('Require unique names for configurations?', 'wc1c-maincore'),
-			'description' => __('If enabled, will need to provide unique names for the configurations.', 'wc1c-maincore'),
+			'label' => esc_html__('Require unique names for configurations?', 'wc1c-maincore'),
+			'description' => esc_html__('If enabled, will need to provide unique names for the configurations.', 'wc1c-maincore'),
 			'default' => 'yes'
 		];
 
 		$fields['configurations_show_per_page'] =
 		[
-			'title' => __('Number in the list', 'wc1c-maincore'),
+			'title' => esc_html__('Number in the list', 'wc1c-maincore'),
 			'type' => 'text',
-			'description' => __('The number of displayed configurations on one page.', 'wc1c-maincore'),
+			'description' => esc_html__('The number of displayed configurations on one page.', 'wc1c-maincore'),
 			'default' => 10,
 			'css' => 'min-width: 20px;',
 		];
 
 		$fields['configurations_draft_delete'] =
 		[
-			'title' => __('Deleting drafts without trash', 'wc1c-maincore'),
+			'title' => esc_html__('Deleting drafts without trash', 'wc1c-maincore'),
 			'type' => 'checkbox',
-			'label' => __('Enable deleting drafts without placing them in the trash?', 'wc1c-maincore'),
-			'description' => __('If enabled, configurations for connections in the draft status will be deleted without being added to the basket.', 'wc1c-maincore'),
+			'label' => esc_html__('Enable deleting drafts without placing them in the trash?', 'wc1c-maincore'),
+			'description' => esc_html__('If enabled, configurations for connections in the draft status will be deleted without being added to the basket.', 'wc1c-maincore'),
 			'default' => 'yes'
 		];
 
@@ -86,22 +86,22 @@ class MainForm extends Form
 	{
 		$fields['technical_title'] =
 		[
-			'title' => __('Technical settings', 'wc1c-maincore'),
+			'title' => esc_html__('Technical settings', 'wc1c-maincore'),
 			'type' => 'title',
-			'description' => __('Used to set up the environment.', 'wc1c-maincore'),
+			'description' => esc_html__('Used to set up the environment.', 'wc1c-maincore'),
 		];
 
 		$fields['php_max_execution_time'] =
 		[
-			'title' => __('Maximum time for execution PHP', 'wc1c-maincore'),
+			'title' => esc_html__('Maximum time for execution PHP', 'wc1c-maincore'),
 			'type' => 'text',
 			'description' => sprintf
 			(
 				'%s <br /> %s <b>%s</b> <br /> %s',
-				__('Value is seconds. WC1C will run until a time limit is set.', 'wc1c-maincore'),
-				__('Server value:', 'wc1c-maincore'),
+                esc_html__('Value is seconds. WC1C will run until a time limit is set.', 'wc1c-maincore'),
+                esc_html__('Server value:', 'wc1c-maincore'),
 				wc1c()->environment()->get('php_max_execution_time'),
-				__('If specify 0, the time limit will be disabled. Specifying 0 is not recommended, it is recommended not to exceed the server limit.', 'wc1c-maincore')
+                esc_html__('If specify 0, the time limit will be disabled. Specifying 0 is not recommended, it is recommended not to exceed the server limit.', 'wc1c-maincore')
 			),
 			'default' => wc1c()->environment()->get('php_max_execution_time'),
 			'css' => 'min-width: 100px;',
@@ -109,15 +109,15 @@ class MainForm extends Form
 
 		$fields['php_post_max_size'] =
         [
-            'title' => __('Maximum request size', 'wc1c-maincore'),
+            'title' => esc_html__('Maximum request size', 'wc1c-maincore'),
             'type' => 'text',
             'description' => sprintf
             (
                 '%s<br />%s <b>%s</b><hr>%s',
-                __('Enter the maximum size of accepted requests at a time in bytes. May be specified with a dimension suffix, such as 7M, where M = megabyte, K = kilobyte, G - gigabyte.', 'wc1c-maincore'),
-                __('Current SERVER limit:', 'wc1c-maincore'),
+                esc_html__('Enter the maximum size of accepted requests at a time in bytes. May be specified with a dimension suffix, such as 7M, where M = megabyte, K = kilobyte, G - gigabyte.', 'wc1c-maincore'),
+                esc_html__('Current SERVER limit:', 'wc1c-maincore'),
                 wc1c()->environment()->get('php_post_max_size'),
-                __('Can only decrease the value, because it must not exceed the limits from the server limit.', 'wc1c-maincore')
+                esc_html__('Can only decrease the value, because it must not exceed the limits from the server limit.', 'wc1c-maincore')
             ),
             'default' => wc1c()->environment()->get('php_post_max_size'),
             'css' => 'min-width: 100px;',
@@ -137,10 +137,10 @@ class MainForm extends Form
 	{
 		$fields['receiver'] =
 		[
-			'title' => __('Receiver', 'wc1c-maincore'),
+			'title' => esc_html__('Receiver', 'wc1c-maincore'),
 			'type' => 'checkbox',
-			'label' => __('Enable data Receiver: background requests?', 'wc1c-maincore'),
-			'description' => __('It is used to receive background requests from 1C in exchange schemes. Do not disable this option if you do not know what it is for.', 'wc1c-maincore'),
+			'label' => esc_html__('Enable data Receiver: background requests?', 'wc1c-maincore'),
+			'description' => esc_html__('It is used to receive background requests from 1C in exchange schemes. Do not disable this option if you do not know what it is for.', 'wc1c-maincore'),
 			'default' => 'yes'
 		];
 
