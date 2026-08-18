0eba4c9b0614776410005186fa6ca7081c4764d3
Frescoref <frescoref@gmail.com>
2026-08-18 21:36:07 +0300

    Fix: more

--- views/helps/bugs.php
+++ views/helps/bugs.php
@@ -21,7 +21,7 @@
 </p>
 
 <p>
-	<a href="<?php echo esc_url_raw(admin_url('admin.php?page=wc1с&section=tools&tool_id=environments')); ?>" class="button">
+	<a href="<?php echo esc_attr(esc_url(admin_url('admin.php?page=wc1c&section=tools&tool_id=environments'))); ?>" class="button">
 		<?php esc_html_e('Environments', 'wc1c-maincore'); ?>
 	</a>
 </p>
